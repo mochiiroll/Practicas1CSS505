1e027472418addbc26ea727f645133180ade53e1
Mariana Hernandez <marianita.hs15gmail.com>
2022-10-20 22:29:31 -0500

    MIS PRACTICAS

---
+++ 13_api/app.js
@@ -0,0 +1,8 @@
+const obtenerFrase=async ()=>{
+    const api=await fetch("https://api.breakingbadquotes.xyz/v1/quotes")
+    const json=await api.json();
+    let frase=document.querySelector("#frase");
+    frase.innerHTML=`
+    <h1>${json[0].quote}</h1>
+    <p>-${json[0].author}</p>`;
+}
